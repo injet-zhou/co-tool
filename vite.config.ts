cbf4a9db025bc82e6cf916f3fb16e66a770e477d
injet-zhou <zhouyingjie@sinkcode.cn>
2022-10-12 00:07:00 +0800

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Icons from 'unplugin-icons/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Icons({})],
+  plugins: [
+    vue(),
+    Icons({}),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar',
+          ],
+        },
+      ],
+    }),
+    Components({ resolvers: [NaiveUiResolver()] }),
+  ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
