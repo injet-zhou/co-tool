7231ab04a80edec8d3b642d6a725b43f71626855
injet-zhou <zhouyingjie@sinkcode.cn>
2022-10-12 00:07:00 +0800

    merge master

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container" />
+</template>
+
+<style scoped>
+.logo.vite:hover {
+  filter: drop-shadow(0 0 2em #747bff);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #249b73);
+}
+</style>
